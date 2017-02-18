7ff0b71f066aa064e52562e0306ff17bdcb1eac3
andela-sakande <suraj.akande@getitllc.com>
2017-02-18 15:34:14 +0100

    Add image intervention

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application([
-    'debug' => true
+    'debug' => true,
+]);
+
+$app->register(new Silex\Provider\DoctrineServiceProvider, [
+    'db.options' => [
+        'driver'   => 'pdo_mysql',
+        'host'     => 'localhost',
+        'dbname'   => 'placekitten',
+        'username' => 'root',
+        'password' => '',
+        'charset'  => 'utf8mb4'
+    ],
 ]);
 
 require __DIR__ . '/../routes/web.php';
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Symfony\Component\HttpFoundation\{Request, Response};
 
-$app->get('/', function (Request $request, Silex\Application $app) {
+$app->get('/{width}/{height}', function (Request $request, Silex\Application $app) {
+
+    $image = $app['db']->fetchAssoc("SELECT filename FROM images ORDER BY rand() LIMIT 1");
+    var_dump($image);
+    die();
     return new Response('Home', 200);
-});
+})->assert('width', '[0-9]+')->assert('height', '[0-9]+');
